d530179b9f2d4833369cb2d5812e92ffdbd6ce86
SirLich <lrkoehler77@gmail.com>
2018-10-05 02:10:35 -0500

    The most recent update. How verbose eh?

--- src/main/me/sirlich/Prison/handlers/PlayerJoinHandler.java
+++ src/main/me/sirlich/Prison/handlers/PlayerJoinHandler.java
@@ -5,12 +5,14 @@ import main.me.sirlich.Prison.core.PlayerState;
 import main.me.sirlich.Prison.core.RpgPlayer;
 import main.me.sirlich.Prison.core.RpgPlayerList;
 import main.me.sirlich.Prison.gates.GateType;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.io.File;
 import java.io.IOException;
@@ -49,6 +51,16 @@ public class PlayerJoinHandler implements Listener
             rpgPlayer.setPlayerState(PlayerState.TUTORIAL);
             player.teleport(Prison.getInstance().getWorldSpawn());
             player.setFlying(false);
+            player.getInventory().setBoots(new ItemStack(Material.IRON_BOOTS));
+            player.getInventory().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
+            player.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
+            player.getInventory().setHelmet(new ItemStack(Material.IRON_HELMET));
+            player.getInventory().addItem(new ItemStack(Material.COOKED_BEEF,30));
+            player.getInventory().addItem(new ItemStack(Material.ARROW,16));
+            player.getInventory().addItem(new ItemStack(Material.IRON_SWORD,1));
+            player.getInventory().addItem(new ItemStack(Material.BOW,1));
+
+
             System.out.println("Attempting to create core-file...");
             if(createPlayerYml(player, playerYml, true)) {
                 System.out.println("Created YML file for: " + player.getName());
